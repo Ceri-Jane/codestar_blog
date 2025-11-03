f8fccdb57a17a7866390c3288755232388e1de7a
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-03 14:20:03 +0000

    deployment settings

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_5sXmu2PdQVHB@ep-blue-wildflower-agcptmun.c-2.eu-central-1.aws.neon.tech/quill_spiny_carry_372163")
+os.environ.setdefault(
+    "SECRET_KEY", "Po0Po0Po0")
